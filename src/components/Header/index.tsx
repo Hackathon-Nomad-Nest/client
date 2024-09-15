3f849a004593a7dc3cbcea62f39bb57c3d20b255
Deep Singh <dev-deepsingh>
2024-09-15 05:54:23 +0530

    Screen: Updates in Home Page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 // import * as React from 'react';
-import logo from '../../Assets/logo/dark.png'
+import logo from '../../Assets/logo/Nomad-edited.png'
 import { Link } from 'react-router-dom';
 import './style.css'
 // import { useGoogleLogin } from '@react-oauth/google';
@@ -15,7 +15,6 @@ const Header = () => {
   //   onSuccess: (tokenResponse) => dispatch(loginUser(tokenResponse.access_token)),
   //   onError: (errorResponse) => console.log(errorResponse),
   // });
-  const theme: string = 'dark';
   const navRoutes = [
     { path: '', label: 'Plans' },
     { path: '', label: 'About' },
@@ -23,17 +22,14 @@ const Header = () => {
   ]
 
   return (<>
-    <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full">
+    <header className="flex glass flex-wrap md:justify-start md:flex-nowrap z-50 w-[98%]  mx-auto mt-3">
       <nav className="relative max-w-7xl w-full flex flex-wrap md:grid md:grid-cols-12 basis-full items-center px-4 md:px-6 md:px-8 mx-auto">
         <div className="logo md:col-span-3">
-          {/* <!-- Logo --> */}
-          <a className="flex-none rounded-xl text-xl inline-block font-semibold focus:outline-none focus:opacity-80" href="../templates/creative-agency/index.html" aria-label="Preline">
-            <img src={logo} alt='' style={{ width: '120px', height: '120px' }} />
-          </a>
-          {/* <!-- End Logo --> */}
+          <Link to='/home' className="flex-none rounded-xl text-xl inline-block font-semibold focus:outline-none focus:opacity-80" aria-label="Preline">
+            <img src={logo} alt='' className='h-[60px] w-[60px]' />
+          </Link>
         </div>
 
-        {/* <!-- Button Group --> */}
         <div className="flex items-center gap-x-1 md:gap-x-2 ms-auto py-1 md:ps-6 md:order-3 md:col-span-3">
           <button type="button" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl bg-white border border-gray-200 text-black hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none">
             Sign in
@@ -46,9 +42,7 @@ const Header = () => {
             </button>
           </div>
         </div>
-        {/* <!-- End Button Group --> */}
 
-        {/* <!-- Collapse --> */}
         <div id="hs-navbar-hcail" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block md:w-auto md:basis-auto md:order-2 md:col-span-6" aria-labelledby="hs-navbar-hcail-collapse">
           <div className="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">
             {
@@ -61,11 +55,9 @@ const Header = () => {
             }
           </div>
         </div>
-        {/* <!-- End Collapse --> */}
       </nav>
     </header>
 
-    {/* section */}
 
   </>
   );
